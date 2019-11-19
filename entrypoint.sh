586a1224c68a9f6e2de0d8de3ee133f613661144
Maikel Koek <maikel@h-o.nl>
2019-11-19 17:31:42 +0100

    change base64 decode

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ main() {
 
     for row in $(echo "${labels}" | jq -r '.[] | @base64'); do
         _jq() {
-         echo ${row} | base64 --decode | jq -r ${1}
+         echo ${row} | base64 -d | jq -r ${1}
         }
 
        echo $(_jq '.name')

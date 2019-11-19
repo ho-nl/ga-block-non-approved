82d9b23e1d25d8c10127506c2af2a959f5c76fa9
Maikel Koek <maikel@h-o.nl>
2019-11-19 18:07:11 +0100

    check if pr contains approved label

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,28 @@ set -o pipefail
 
 main() {
     approvedLabels=$INPUT_LABELS
-    echo ${approvedLabels}
+    IFS=', ' read -r -a approvedLabels <<< "$approvedLabels"
 
     labels=$(jq -r .pull_request.labels "$GITHUB_EVENT_PATH")
 #    labels=$(jq -r .pull_request.labels "test.json");
 
+    approved=false
     for row in $(echo "${labels}" | jq -r '.[] | @base64'); do
         _jq() {
          echo ${row} | base64 -d | jq -r ${1}
         }
 
-       echo $(_jq '.name')
+        label=$(_jq '.name')
+
+        if [[ " ${approvedLabels[@]} " =~ " ${label} " ]]; then
+            flag=true
+        fi
     done
 }
+
+if (! ${flag}); then
+    echo 'Pull request is not approved'
+    exit 1
+fi
 
 main
